d9e3a1334ba9eed6286d1d2183b4bfc1bb1adcec
Merline Belizaire <merline@noukod.com>
2019-02-21 22:00:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function prependDriver(name){
 }
 
 function removeLastDriver(){
-        const drivers.slice(0,drivers.length-1);
-
+        const array = drivers.slice(0,drivers.length-1);
+      
 }
-
+      
       function removeFirstDriver(){
-        const drivers.slice(1,drivers.length);
+        const array = drivers.slice(1,drivers.length);    
 }
