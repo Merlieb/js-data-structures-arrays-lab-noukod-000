7765d03e75a56004505114b1b78fdc54b63cc30f
Merline Belizaire <merline@noukod.com>
2019-02-21 22:00:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,15 +19,17 @@ function destructivelyRemoveFirstDriver(name){
 
 function appendDriver(name){
   return [...drivers, name]
-        }
+}
 
 function prependDriver(name){
         return [name, ...drivers]
-      }
-
-
-function removeLastDriver(){
-    return  drivers.slice(-1)
 }
 
-drivers
+function removeLastDriver(){
+        const drivers.slice(0,drivers.length-1);
+      
+}
+      
+      function removeFirstDriver(){
+        const drivers.slice(1,drivers.length);    
+}
